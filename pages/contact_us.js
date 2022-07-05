a94456fa5faa02a426c44528f63e905a45c02bd0
Primal <81525899+Seun2255@users.noreply.github.com>
2022-07-05 06:09:49 +0100

    feature: contact Us page

---
+++ pages/contact_us.js
@@ -0,0 +1,81 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/contact_us.module.css";
+import logo from "../assets/logo.png";
+import background from "../assets/contact.png";
+import arrow from "../assets/arrow-drop-down.svg";
+import youTube from "../assets/icons/youtube-2.svg";
+import instagram from "../assets/icons/instagram.svg";
+import facebook from "../assets/icons/facebook.svg";
+import twitter from "../assets/icons/twitter.svg";
+import linkedIn from "../assets/icons/linkedin.svg";
+
+export default function Contact() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <div className={styles.background__image}>
+          <Image alt="background" layout="fill" src={background} />
+        </div>
+        <Link href={"/"}>
+          <div className={styles.logo}>
+            <Image alt="logo" layout="fill" src={logo} />
+          </div>
+        </Link>
+        <div className={styles.home__buttons}>
+          <button className={styles.menu__button}>
+            <span>Menu</span>
+            <div className={styles.dropdown__arrow}>
+              <Image alt="arrow" layout="fill" src={arrow} />
+            </div>
+          </button>
+          <button className={styles.menu__button}>
+            <span>FAQs</span>
+            <div className={styles.dropdown__arrow}>
+              <Image alt="arrow" layout="fill" src={arrow} />
+            </div>
+          </button>
+          <button className={styles.menu__button}>
+            <span className={styles.large__text}>Query Categories</span>
+            <div className={styles.dropdown__arrow}>
+              <Image alt="arrow" layout="fill" src={arrow} />
+            </div>
+          </button>
+          <button className={styles.menu__button__plain}>Track Query</button>
+          <button className={styles.menu__button__plain}>Feedback</button>
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <div className={styles.social__links}>
+          <div className={styles.icon}>
+            <Image alt="arrow" layout="fill" src={youTube} />
+          </div>
+          <div className={styles.icon}>
+            <Image alt="arrow" layout="fill" src={twitter} />
+          </div>
+          <div className={styles.icon}>
+            <Image alt="arrow" layout="fill" src={linkedIn} />
+          </div>
+          <div className={styles.icon}>
+            <Image alt="arrow" layout="fill" src={instagram} />
+          </div>
+          <div className={styles.icon}>
+            <Image alt="arrow" layout="fill" src={facebook} />
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+{
+  /* <i
+                class="fa-brands fa-twitter"
+                style="
+                  color: white;
+                  width: 20px;
+                  height: 20px;
+                  margin-right: 10px;
+                "
+              ></i> */
+}
