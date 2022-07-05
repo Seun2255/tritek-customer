32242709f776d409dc560efac6932968903daf4c
Primal <81525899+Seun2255@users.noreply.github.com>
2022-07-05 17:19:22 +0100

    feature: FAQ page

---
+++ pages/FAQ.js
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/faq.module.css";
+import logo from "../assets/logo.png";
+import background from "../assets/faq.png";
+import arrow from "../assets/arrow-drop-down.svg";
+import youTube from "../assets/icons/youtube-2.svg";
+import instagram from "../assets/icons/instagram.svg";
+import facebook from "../assets/icons/facebook.svg";
+import twitter from "../assets/icons/twitter.svg";
+import linkedIn from "../assets/icons/linkedin.svg";
+
+export default function FAQ() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const questions = [
+    "Q. What does Tritek consulting do?",
+    "Q. Do you offer practical experience",
+    "Q. Can i pay for my course instalmentally?",
+    "Q. What is the cost of the program",
+    "Q. What is the mode of training",
+  ];
+
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <div className={styles.background__image}>
+          <Image alt="background" layout="fill" src={background} />
+        </div>
+        <Link href={"/"}>
+          <div className={styles.logo}>
+            <Image alt="logo" layout="fill" src={logo} />
+          </div>
+        </Link>
+        <div className={styles.home__buttons}>
+          <button
+            className={styles.menu__button}
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <span>FAQs</span>
+            <div className={styles.dropdown__arrow}>
+              <Image
+                alt="arrow"
+                layout="fill"
+                src={arrow}
+                style={{
+                  transform: menuOpen ? "rotate(180deg)" : "rotate(0deg)",
+                }}
+              />
+            </div>
+          </button>
+          {menuOpen && (
+            <div className={styles.menu}>
+              {questions.map((question, id) => {
+                return (
+                  <div key={id} className={styles.question}>
+                    {question}
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <div className={styles.social__links}>
+          <div className={styles.icon}>
+            <Image alt="arrow" layout="fill" src={youTube} />
+          </div>
+          <div className={styles.icon}>
+            <Image alt="arrow" layout="fill" src={twitter} />
+          </div>
+          <div className={styles.icon}>
+            <Image alt="arrow" layout="fill" src={linkedIn} />
+          </div>
+          <div className={styles.icon}>
+            <Image alt="arrow" layout="fill" src={instagram} />
+          </div>
+          <div className={styles.icon}>
+            <Image alt="arrow" layout="fill" src={facebook} />
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
