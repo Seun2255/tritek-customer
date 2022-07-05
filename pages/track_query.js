f34826a98a2f2d4750ec9ab73f090ea751b77a18
Primal <81525899+Seun2255@users.noreply.github.com>
2022-07-05 17:21:24 +0100

    eature: track query page

---
+++ pages/track_query.js
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/track_query.module.css";
+import logo from "../assets/logo.png";
+import background from "../assets/track_query.png";
+import arrow from "../assets/arrow-drop-down.svg";
+import youTube from "../assets/icons/youtube-2.svg";
+import instagram from "../assets/icons/instagram.svg";
+import facebook from "../assets/icons/facebook.svg";
+import twitter from "../assets/icons/twitter.svg";
+import linkedIn from "../assets/icons/linkedin.svg";
+
+export default function Track_query() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <div className={styles.background__image}>
+          <Image alt="background" layout="fill" src={background} />
+        </div>
+        <Link href={"/"}>
+          <div className={styles.logo}>
+            <Image alt="logo" layout="fill" src={logo} />
+          </div>
+        </Link>
+        <div className={styles.home__buttons}>
+          <button className={styles.menu__button__plain}>
+            Track Query: Eg. TRI2004651229 TEK
+          </button>
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <div className={styles.social__links}>
+          <div className={styles.icon}>
+            <Image alt="arrow" layout="fill" src={youTube} />
+          </div>
+          <div className={styles.icon}>
+            <Image alt="arrow" layout="fill" src={twitter} />
+          </div>
+          <div className={styles.icon}>
+            <Image alt="arrow" layout="fill" src={linkedIn} />
+          </div>
+          <div className={styles.icon}>
+            <Image alt="arrow" layout="fill" src={instagram} />
+          </div>
+          <div className={styles.icon}>
+            <Image alt="arrow" layout="fill" src={facebook} />
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
